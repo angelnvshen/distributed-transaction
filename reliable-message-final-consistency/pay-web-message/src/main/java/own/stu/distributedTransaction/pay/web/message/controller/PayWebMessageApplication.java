d6382187a21ccdfe03e1f6091e20525599e4bbdd
angelnvshen <angelnvshen@163.com>
2018-05-09 17:21:20 +0800

    add enable robbin client

--- reliable-message-final-consistency/pay-web-message/src/main/java/own/stu/distributedTransaction/pay/web/message/controller/PayWebMessageApplication.java
+++ reliable-message-final-consistency/pay-web-message/src/main/java/own/stu/distributedTransaction/pay/web/message/controller/PayWebMessageApplication.java
@@ -3,13 +3,20 @@ package own.stu.distributedTransaction.pay.web.message.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tk.mybatis.spring.annotation.MapperScan;
+
+import java.io.File;
+import java.net.URL;
 
 /**
  * Created by CHANEL on 2018/5/6.
@@ -36,8 +43,15 @@ public class PayWebMessageApplication extends SpringBootServletInitializer {
         return "redirect:login";
     }
 
-   /* @Bean
-    public JmsTemplate getJmsTemplate(){
-        return new JmsTemplate();
-    }*/
+    /* 仅仅 内置tomcat使用*/
+    @Bean
+    public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
+        ConfigurableEmbeddedServletContainer factory = new TomcatEmbeddedServletContainerFactory();
+
+        URL xmlPath = this.getClass().getClassLoader().getResource("");
+        String customer_jsp_path = xmlPath.getFile().replaceAll("target/classes", "src/main/webapp");
+
+        factory.setDocumentRoot(new File(customer_jsp_path));
+        return (EmbeddedServletContainerFactory) factory;
+    }
 }
